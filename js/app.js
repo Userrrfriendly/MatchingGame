6786c487a1401df55a3f81c6e26bf9bfe2ccc0fb
Veniamin Tsigourof <tsigourof_ben@hotmail.com>
2018-04-12 16:48:14 +0300

    Cleaned and refactored Code.

--- js/app.js
+++ js/app.js
@@ -3,17 +3,12 @@ let cardArray = ["fa-diamond", "fa-paper-plane-o", "fa-anchor", "fa-bolt",
     "fa-paper-plane-o", "fa-anchor", "fa-bolt", "fa-cube", "fa-leaf", "fa-bicycle", "fa-bomb"
 ];
 const deckContainer = document.querySelector('.deck-container');
-const container = document.querySelector('.container'); //i think its redundant so far
 const restartIcon = document.querySelector('.fa-repeat');
 const timerDisplay = document.querySelector('.timer');
+const movesDisplay = document.querySelector('.moves');
 const scorePanel = document.querySelector('.score-panel');
 const gameOverScreen = document.querySelector('.victory-screen');
-const victoryScore = document.querySelector('.wrapper');
-//any reason why you didnt use querSelectorAll to catch em all? REFACTOR?
-const stars = [document.querySelector('.stars').firstElementChild.firstElementChild,
-    document.querySelector('.stars').firstElementChild.nextElementSibling.firstElementChild,
-    document.querySelector('.stars').lastElementChild.firstElementChild
-];
+const stars = document.querySelectorAll('.fa-star');
 let game = {
     state: '',
     openedCard: '',
@@ -24,16 +19,17 @@ let game = {
     totalTime: 0
 };
 let timeoutID;
-let count = 1;
+let countSeconds = 1;
 let clickBlock = false;
+
 //this "checks" which Animation End handle the browser accept
-// and assigns it in the constant animationEnd - See https://github.com/daneden/animate.css/issues/644 
+// and assigns it in the constant animationEnd - See https://github.com/daneden/animate.css/issues/644
 const animationEnd = (function (el) {
     const animations = {
         animation: 'animationend',
         OAnimation: 'oAnimationEnd',
         MozAnimation: 'mozAnimationEnd',
-        WebkitAnimation: 'webkitAnimationEnd',
+        WebkitAnimation: 'webkitAnimationEnd'
     };
 
     for (let t in animations) {
@@ -43,6 +39,77 @@ const animationEnd = (function (el) {
     }
 })(document.createElement('div'));
 
+// Shuffle function from http://stackoverflow.com/a/2450976
+function shuffle(array) {
+    var currentIndex = array.length,
+        temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+/* makeCard() returns the following:
+<section class="card-container animated">
+	<div class="card">
+		<figure class="card-figure front"></figure>
+		<figure class="card-figure back">
+			<i class="fa fa-leaf card-icon"></i>
+		</figure>
+	</div>
+</section>
+*/
+function makeCard(cardArr, cardArrayIndex) {
+    let section = document.createElement('section');
+    let divCard = document.createElement('div');
+    let figureFront = document.createElement('figure');
+    let icon = document.createElement('i');
+    let figureBack = document.createElement('figure');
+
+    section.classList.add('card-container', 'animated');
+    divCard.classList.add('card');
+    figureFront.classList.add('card-figure', 'front');
+    icon.classList.add('fa', cardArr[cardArrayIndex], 'card-icon');
+    figureBack.classList.add('card-figure', 'back');
+
+    figureBack.appendChild(icon);
+    divCard.appendChild(figureFront);
+    divCard.appendChild(figureBack);
+    section.appendChild(divCard);
+
+    return section;
+}
+
+function makeDeck(array) {
+    let deck = document.createElement('div');
+    deck.classList.add('deck');
+    let divFragment = document.createDocumentFragment();
+    for (let i = 0; i < array.length; i++) {
+        divFragment.appendChild(makeCard(cardArray, i));
+    }
+    deck.appendChild(divFragment);
+    return deck;
+}
+
+function renderDeck(array) {
+    const deck = document.querySelector('.deck');
+    if (deck) {
+        deck.remove();
+        shuffle(array);
+        deckContainer.appendChild(makeDeck(array));
+    } else {
+        shuffle(array);
+        deckContainer.appendChild(makeDeck(array));
+    }
+    resetGame();
+}
+
 function resetGame() {
     game.state = 'ready';
     game.openedCard = '';
@@ -51,64 +118,64 @@ function resetGame() {
     game.matchedCards = 0;
     game.score = 3;
     game.totalTime = 0;
-    document.querySelector('.moves').textContent = game.moves;
-    document.querySelector('.timer').textContent = game.totalTime
+    movesDisplay.textContent = game.moves;
+    timerDisplay.textContent = game.totalTime
     for (let i = 1; i < 3; i++) {
         stars[i].classList.remove('faded-star');
     }
-    count = 0;
+    countSeconds = 0;
     updateTime();
     clock(false);
     clickBlock = false;
 }
 
-function updateState(node, iconClass) {
+renderDeck(cardArray);
+
+function updateState(el, iconClass) {
+    //ready= no moves yet but the game can start by flipping a card (moves === 0)
+    //live= at least one card has been flipped (moves >= 1)
+    //open-card=there is a flipped card on the board
     switch (game.state) {
-        //ready= no moves yet but the game can start by flipping a card (moves === 0)
-        //live= at least one card has been flipped (moves >= 1)
-        //open-card=there is a flipped card on the board
         case 'ready':
-            flipElement(node);
-            node.classList.add('open');
+            flipElement(el);
+            el.classList.add('open');
             game.state = 'open-card';
-            game.openedCard = node;
+            game.openedCard = el;
             game.iconClass = iconClass;
             game.moves += 1;
-            score();
-            document.querySelector('.moves').textContent = game.moves;
+            updateScore();
+            movesDisplay.textContent = game.moves; //cas it
             updateTime();
             clock(true);
             break;
         case 'live':
-            flipElement(node);
-            node.classList.add('open');
+            flipElement(el);
+            el.classList.add('open');
             game.state = 'open-card';
-            game.openedCard = node;
+            game.openedCard = el;
             game.iconClass = iconClass;
             game.moves += 1;
-            score();
-            document.querySelector('.moves').textContent = game.moves;
+            updateScore();
+            movesDisplay.textContent = game.moves;
             break;
         case 'open-card':
-            flipElement(node);
+            flipElement(el);
             if (game.iconClass === iconClass) {
-                node.classList.add('matched', 'open');
+                el.classList.add('matched', 'open');
                 game.openedCard.classList.add('matched');
-
-                node.parentElement.classList.add('tada');
+                el.parentElement.classList.add('tada');
                 game.openedCard.parentElement.classList.add('tada');
-
                 game.state = 'live';
                 game.openedCard = '';
                 game.iconClass = '';
                 game.matchedCards === 7 ? gameOver() : game.matchedCards += 1;
             } else {
                 game.openedCard.classList.remove('open');
-                const tempGameValue = game.openedCard //it is here because of the setTimeout
+                const tempGameValue = game.openedCard;
                 setTimeout(function () {
                     tempGameValue.classList.remove('flipped');
-                    node.classList.remove('flipped');
-                }, 600)
+                    el.classList.remove('flipped');
+                }, 600);
                 game.openedCard = '';
                 game.state = 'live';
                 game.iconClass = '';
@@ -117,8 +184,8 @@ function updateState(node, iconClass) {
     }
 }
 
-function flipElement(node) {
-    node.classList.toggle('flipped');
+function flipElement(el) {
+    el.classList.toggle('flipped');
 }
 
 function getIconClass(classList) {
@@ -132,13 +199,12 @@ function getIconClass(classList) {
 }
 
 function gameOver() {
-    console.log('GAME OVER');
-    game.totalTime = count;
+    game.totalTime = countSeconds;
     clock(false);
     victoryScreen();
 }
 
-function score() {
+function updateScore() {
     if (game.moves > 15 && game.moves < 26) {
         game.score = 2
         stars[2].classList.add('faded-star');
@@ -158,189 +224,21 @@ function clock(bool) {
 
     function stopTimer() {
         clearInterval(timeoutID);
-        count = 1;
+        countSeconds = 1;
     }
 }
 
 function updateTime() {
-    if (count >= 60) {
-        const mins = Math.trunc(count / 60);
-        const secs = count - mins * 60;
+    if (countSeconds >= 60) {
+        const mins = Math.trunc(countSeconds / 60);
+        const secs = countSeconds - mins * 60;
         secs < 10 ? timerDisplay.textContent = mins + ':0' + secs : timerDisplay.textContent = mins + ':' + secs;
-        count += 1;
+        countSeconds += 1;
     } else {
-        count < 10 ? timerDisplay.textContent = '00:0' + count : timerDisplay.textContent = '00:' + count;
-        count += 1;
+        countSeconds < 10 ? timerDisplay.textContent = '00:0' + countSeconds : timerDisplay.textContent = '00:' + countSeconds;
+        countSeconds += 1;
     }
 }
-
-// Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
-    var currentIndex = array.length,
-        temporaryValue, randomIndex;
-
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-
-    return array;
-}
-
-/* makeDeck() creates the following block of elements:
-<div class="deck">
-    <section class="card-container animated">
-        <div class="card">
-            <figure class="card-figure front"></figure>
-            <figure class="card-figure back">
-                <i class="fa fa-leaf card-icon"></i>
-            </figure>
-        </div>
-    </section>
-    ...
-    ...
-</div>
-*/
-
-/* makeCard() creates:
-<section class="card-container animated">
-	<div class="card">
-		<figure class="card-figure front"></figure>
-		<figure class="card-figure back">
-			<i class="fa fa-leaf card-icon"></i>
-		</figure>
-	</div>
-</section>
-*/
-
-function makeDeck() {
-    let deck = document.createElement('div');
-    deck.classList.add('deck');
-    let divFragment = document.createDocumentFragment();
-    for (let i = 0; i < cardArray.length; i++) {
-        divFragment.appendChild(makeCard(i));
-    }
-    deck.appendChild(divFragment);
-    return deck;
-}
-
-function makeCard(cardArrayIndex) {
-    //YOU CAN TRY AND REFACTORE IT WITH Node.cloneNode();
-    /*
-        -backFigure.appendChild(icon)
-        -divCard.append(frontfigure)
-        -divCard.append(backfigure)
-        -section.append(divCard)
-
-        -divDeck.append(divCard)
-            
-     */
-    let section = document.createElement('section');
-    section.classList.add('card-container', 'animated');
-
-    let divCard = document.createElement('div');
-    divCard.classList.add('card');
-
-    let figureFront = document.createElement('figure');
-    figureFront.classList.add('card-figure', 'front');
-    let icon = document.createElement('i');
-    icon.classList.add('fa', cardArray[cardArrayIndex], 'card-icon');
-    let figureBack = document.createElement('figure');
-    figureBack.classList.add('card-figure', 'back');
-
-    figureBack.appendChild(icon);
-    divCard.appendChild(figureFront);
-    divCard.appendChild(figureBack);
-    section.appendChild(divCard);
-
-    return section;
-}
-
-function renderDeck() {
-    const deck = document.querySelector('.deck');
-    if (deck) {
-        deck.remove();
-        shuffle(cardArray);
-        deckContainer.appendChild(makeDeck());
-    } else {
-        shuffle(cardArray);
-        deckContainer.appendChild(makeDeck());
-    }
-    resetGame();
-}
-
-renderDeck();
-
-// EVENT LISTENERES
-deckContainer.addEventListener('click', cardClick);
-
-function cardClick(e) {
-    if (e.target.nodeName === 'FIGURE' && e.target.classList.contains('front') && clickBlock === false) {
-        if (!e.target.parentElement.classList.contains('open')) {
-            //if a card is opened disable click untill the animations ends and all cards are facedown
-            if (game.state === 'open-card') {
-                clickBlock = true;
-                let stopClickID = window.setTimeout(stopClick, 600);
-
-                function stopClick() {
-                    window.clearTimeout(stopClickID);
-                    clickBlock = false;
-                }
-            }
-            updateState(e.target.parentElement, getIconClass(e.target.nextElementSibling.firstElementChild.classList));
-        }
-    }
-}
-
-// const restartIcon2 = document.querySelector('.restart');
-restartIcon.addEventListener('click', function (e) {
-    rotateElement(e);
-    renderDeck();
-});
-
-function rotateElement(e) {
-    if (!e.target.classList.contains('rotate')) {
-        e.target.classList.add('rotate');
-        e.target.parentElement.classList.remove('rotate');
-    } else {
-        e.target.classList.remove('rotate');
-        e.target.parentElement.classList.add('rotate');
-    }
-}
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move countSecondser and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
-
-/* TODO: 
-    *Arrange order of code to ensure redability
-
-REFACTIRING:
-*some of your functions are generic (flipCard) and some are specific (resetAnimation)
-*check for e.target vs e.currentTarget(targets the event that has the event listener)
---Positioned icons inside card (vertical alignment) with vw- check the original udacity file (just for shits and giggles)
-
-*/
-
-
-
-gameOverScreen.addEventListener(animationEnd, resetAnimation);
-
-function resetAnimation(e) {
-    //"resets" the element so it can be animated again with a different animation effect
-    gameOverScreen.classList.toggle('displaced');
-    gameOverScreen.classList.remove('fadeInDown', 'fadeOutUp');
-};
 
 function victoryScreen() {
     const scoreClone = scorePanel.firstElementChild.cloneNode(true);
@@ -356,8 +254,54 @@ function fadeOutUp() {
     gameOverScreen.classList.add('fadeOutUp');
 };
 
+function rotateElementZ(e) {
+    //rotates target element around its z axis by adding the class .rotate
+    //target element requires to be wrapped in a 'container' element
+    if (!e.target.classList.contains('rotate')) {
+        e.target.classList.add('rotate');
+        e.target.parentElement.classList.remove('rotate');
+    } else {
+        e.target.classList.remove('rotate');
+        e.target.parentElement.classList.add('rotate');
+    }
+}
+
+/*EVENT LISTENERES */
+deckContainer.addEventListener('click', cardClick);
+
+function cardClick(e) {
+    if (e.target.nodeName === 'FIGURE' && e.target.classList.contains('front') && clickBlock === false) {
+        if (!e.target.parentElement.classList.contains('open')) {
+            if (game.state === 'open-card') {
+                //if a card is opened disable click untill the animations ends and all cards are facedown
+                clickBlock = true;
+                let stopClickID = window.setTimeout(stopClick, 600);
+
+                function stopClick() {
+                    window.clearTimeout(stopClickID);
+                    clickBlock = false;
+                }
+            }
+            updateState(e.target.parentElement, getIconClass(e.target.nextElementSibling.firstElementChild.classList));
+        }
+    }
+}
+
+restartIcon.addEventListener('click', function (e) {
+    rotateElementZ(e);
+    renderDeck(cardArray);
+});
+
+gameOverScreen.addEventListener(animationEnd, resetAnimation);
+
+function resetAnimation(e) {
+    //"resets" the element so it can be animated again with a different animation effect
+    gameOverScreen.classList.toggle('displaced');
+    gameOverScreen.classList.remove('fadeInDown', 'fadeOutUp');
+};
+
 document.querySelector('.new-game').addEventListener('click', function () {
     fadeOutUp();
     document.querySelector('.score-stars').remove();
-    renderDeck();
+    renderDeck(cardArray);
 });
